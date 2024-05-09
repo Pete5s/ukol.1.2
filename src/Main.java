15b01a06ad31e141881c2e20b93e0a0bac565e07
Petr S <Onlythis1981@hotmail.com>
2024-05-09 02:14:24 +0200

    1.2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        Prodejce prodejce = new Prodejce("Vincent Mažňák", "09.11.1975", 5, 10, "Praha", "4S39842", 6.7, "192.168.0.1");
+        Prodejce Prodejce = new Prodejce("Donald Vega", "02.03.1985", 3, 6, "Beroun", "5B9654", 4.9, "192.168.0.1");
+
+        double prumerMrkveNaSmlouvu = prodejce.mnozstviMrkveVTunach / prodejce.pocetSmluv;
+
+        System.out.println("Průměrné množství prodané mrkve na jednu smlouvu: " + prumerMrkveNaSmlouvu + " tun.");
+    }
+}
+
+class Prodejce {
+    String jmenoAPrijmeni;
+    String datumNarozeni;
+    int pocetSmluv;
+    int mnozstviMrkveVTunach;
+    String nazevMesta;
+    String spzVozidla;
+    double spotrebaVozidlaNa100Km;
+    String ipAdresaPocitace;
+
+    public Prodejce(String jmenoAPrijmeni, String datumNarozeni, int pocetSmluv, int mnozstviMrkveVTunach,
+                    String nazevMesta, String spzVozidla, double spotrebaVozidlaNa100Km, String ipAdresaPocitace) {
+
+        this.jmenoAPrijmeni = jmenoAPrijmeni;
+        this.datumNarozeni = datumNarozeni;
+        this.pocetSmluv = pocetSmluv;
+        this.mnozstviMrkveVTunach = mnozstviMrkveVTunach;
+        this.nazevMesta = nazevMesta;
+        this.spzVozidla = spzVozidla;
+        this.spotrebaVozidlaNa100Km = spotrebaVozidlaNa100Km;
+        this.ipAdresaPocitace = ipAdresaPocitace;
     }
 }
